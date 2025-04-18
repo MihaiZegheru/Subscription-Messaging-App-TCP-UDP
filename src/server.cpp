5de086c5792a5a7b3ce7615c6b7653ce202ce161
Mihai Zegheru <mihai@mihaizegheru.com>
2025-04-18 15:08:33 +0300

    Added set_fd for server.
    Signed-off-by: Mihai Zegheru <mihai@mihaizegheru.com>

--- src/server.cpp
+++ src/server.cpp
@@ -20,6 +20,21 @@
 #define CHECK(exp, msg) assert((void(msg), !(exp)))
 
 const int kMaxClients = 5;
+const int kBuffLen = 256;
+
+char buff[kBuffLen];
+
+void HandleSTDIN(bool &isRunning) {
+    fgets(buff, kBuffLen - 1, stdin);
+    if (!strcmp(buff, "exit\n")) {
+        isRunning = false;
+
+        // TODO: Send exit message to clients
+
+    } else {
+        std::cout << "Command does not exist.\n";
+    }
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -60,8 +75,40 @@ int main(int argc, char *argv[]) {
     rc = listen(sockfdTCP, kMaxClients);
     CHECK(rc < 0, "listen");
 
-    while (true) {
+    // Initialise fd_set
+    fd_set fds;
 
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds);
+    FD_SET(sockfdUDP, &fds);
+    FD_SET(sockfdTCP, &fds);
+
+    int fdmax = std::max(sockfdUDP, sockfdTCP);
+
+    bool isRunning = true;
+
+    while (isRunning) {
+        memset(buff, 0, kBuffLen);
+
+        // Wait until fd is used
+        rc = select(fdmax + 1, &fds, NULL, NULL, NULL);
+        CHECK(rc < 0, "select");
+
+        for (int fd = 0; fd <= fdmax; fd++) {
+            if (!FD_ISSET(fd, &fds)) {
+                continue;
+            }
+
+            if (fd == STDIN_FILENO) {
+                HandleSTDIN(isRunning);
+            } else if (fd == sockfdUDP) {
+
+            } else if (fd == sockfdTCP) {
+
+            } else {
+
+            }
+        }
     }
 
     // Close sockets
