7e910cf0ec01c583d72457dcd7362c8a474b9528
Mihai Zegheru <mihai@mihaizegheru.com>
2025-04-20 12:14:49 +0300

    Refactored subscriber.cpp keeping the same logic. Left boilerplate main.
    Signed-off-by: Mihai Zegheru <mihai@mihaizegheru.com>

--- src/subscriber.cpp
+++ src/subscriber.cpp
@@ -28,39 +28,120 @@ char buff[kBuffLen];
 
 namespace subscriber {
 
+namespace {
+
+void SendExit(int sockfd) {
+    std::string message = "ext";
+    strcpy(buff, message.c_str());
+    int rc = send_all(sockfd, buff, kBuffLen);
+    CHECK(rc < 0, "send");
+}
+
+void SendSubscribe(int sockfd, const std::string topic) {
+    std::string message = "sub$" + topic;
+    strcpy(buff, message.c_str());
+    int rc = send_all(sockfd, buff, kBuffLen);
+    CHECK(rc < 0, "send");
+    LOG_INFO("Subscribed to topic " << topic);
+}
+
+void SendUnsubscribe(int sockfd, const std::string topic) {
+    std::string message = "uns$" + topic;
+    strcpy(buff, message.c_str());
+    int rc = send_all(sockfd, buff, kBuffLen);
+    CHECK(rc < 0, "send");
+    LOG_INFO("Unsubscribed from topic " << topic);
+}
+
+void RecvBufferToPublishedMessage(char *src, PublishedMessage &message) {
+    size_t offset = 0;
+    offset += 3;
+    memcpy(&message.ip, src + offset, sizeof(message.ip));
+    offset += sizeof(message.ip);
+    memcpy(&message.port, src + offset, sizeof(message.port));
+    offset += sizeof(message.port);
+
+    message.topic = std::string(src + offset, kTopicLen);
+    message.topic.erase(message.topic.find_last_not_of('\0') + 1);
+    offset += kTopicLen;
+    message.type = src[offset];
+    offset += 1;
+    memcpy(message.content, src + offset, kMaxContentLen);
+}
+
+// TODO: Convert std::string type to std::stringview.
+//       Change all const global strings.
+void PrettyPrintMessage(const std::string topic,
+                        const std::string type,
+                        const std::string content) {
+    std::stringstream ss;
+    ss << topic << " - " << type << " - " << content;
+    LOG_INFO(ss.str());
+}
+
+void PrettyPrintIntMessage(const PublishedMessage message) {
+    uint32_t num;
+    memcpy(&num, message.content + 1, sizeof(num));
+    std::string content(std::to_string(
+            (int64_t)ntohl(num) * (message.content[0] == 1 ? -1 : 1)));
+    PrettyPrintMessage(std::move(message.topic),
+                       std::move(kIntType),
+                       std::move(content));
+}
+
+void PrettyPrintShortRealMessage(const PublishedMessage message) {
+    uint16_t num;
+    memcpy(&num, message.content, sizeof(num));
+    std::stringstream ss;
+    ss.precision(2);
+    ss << std::fixed << (double)ntohs(num) / 100;
+    std::string content = ss.str();
+    PrettyPrintMessage(std::move(message.topic),
+                       std::move(kShortRealType),
+                       std::move(content));
+}
+
+void PrettyPrintFloatMessage(const PublishedMessage message) {
+    uint32_t num;
+    memcpy(&num, message.content + 1, sizeof(num));
+    uint8_t exp;
+    memcpy(&exp, message.content + 1 + sizeof(num), sizeof(exp));
+    std::stringstream ss;
+    ss.precision(10);
+    ss << std::fixed
+       << (double)ntohl(num) / std::pow(10, exp) * 
+          (message.content[0] == 1 ? -1 : 1);
+    std::string content = ss.str();
+    PrettyPrintMessage(std::move(message.topic),
+                       std::move(kFloatType),
+                       std::move(content));
+}
+
+void PrettyPrintStringMessage(const PublishedMessage message) {
+    std::string content = std::string(message.content, kMaxContentLen);     
+    PrettyPrintMessage(std::move(message.topic),
+                       std::move(kStringType),
+                       std::move(content));
+}
+
+} // namespace
+
 /**
  * May modify reference parameter isRunning.
 */
 void HandleSTDIN(int sockfd, bool &isRunning) {
-    int rc;
-
     std::string input;
     std::cin >> input;
 
     if (input == "exit") {
         isRunning = false;
-        std::string message = "ext";
-        strcpy(buff, message.c_str());
-        LOG_DEBUG("HELL");
-        rc = send_all(sockfd, buff, kBuffLen);
-        LOG_DEBUG("HELL");
-        CHECK(rc < 0, "send");
+        SendExit(sockfd);
     } else if (input == "subscribe") {
         std::cin >> input;
-        std::string message = "sub$" + input;
-        strcpy(buff, message.c_str());
-        rc = send_all(sockfd, buff, kBuffLen);
-        CHECK(rc < 0, "send");
-
-        LOG_INFO("Subscribed to topic " << input);
+        SendSubscribe(sockfd, std::move(input));
     } else if (input == "unsubscribe") {
         std::cin >> input;
-        std::string message = "uns$" + input;
-        strcpy(buff, message.c_str());
-        rc = send_all(sockfd, buff, kBuffLen);
-        CHECK(rc < 0, "send");
-
-        LOG_INFO("Unsubscribed from topic " << input);
+        SendUnsubscribe(sockfd, std::move(input));
     } else {
         LOG_DEBUG("Command does not exist.\n");
     }
@@ -77,64 +158,25 @@ void HandleTCP(int sockfd, bool &isRunning) {
         isRunning = false;
     } else if (std::string(buff, 3) == "msg") {
         PublishedMessage message;
-
-        size_t offset = 0;
-        offset += 3;
-        memcpy(&message.ip, buff + offset, sizeof(message.ip));
-        offset += sizeof(message.ip);
-        memcpy(&message.port, buff + offset, sizeof(message.port));
-        offset += sizeof(message.port);
-
-        message.topic = std::string(buff + offset, kTopicLen);
-        message.topic.erase(message.topic.find_last_not_of('\0') + 1);
-        offset += kTopicLen;
-        message.type = buff[offset];
-        offset += 1;
-        memcpy(message.content, buff + offset, kMaxContentLen);
-
-        std::stringstream ss;
-        ss << message.topic;
-
-        int sign = 1;
+        RecvBufferToPublishedMessage(buff, message);
         switch (message.type) {
             case 0:
-                uint32_t numI;
-                sign = message.content[0] == 1 ? -1 : 1;
-                memcpy(&numI, message.content + 1, sizeof(numI));
-                LOG_DEBUG(sign);
-                ss << " - " << kIntType
-                   << " - " << std::to_string((int64_t)ntohl(numI) * sign);
+                PrettyPrintIntMessage(std::move(message));
                 break;
             case 1:
-                uint16_t numSR;
-                memcpy(&numSR, message.content, sizeof(numSR));
-                ss.precision(2);
-                ss << " - " << kShortRealType
-                   << " - " << std::fixed
-                   << (double)ntohs(numSR) / 100;
+                PrettyPrintShortRealMessage(std::move(message));
                 break;
             case 2:
-                uint32_t numF;
-                memcpy(&numF, message.content + 1, sizeof(numF));
-                uint8_t exp;
-                memcpy(&exp, message.content + 1 + sizeof(numF), sizeof(exp));
-                sign = message.content[0] == 1 ? -1 : 1;
-                ss.precision(10);
-                ss << " - " << kFloatType
-                   << " - " << std::fixed
-                   << (double)ntohl(numF) / std::pow(10, exp) * sign;
+                PrettyPrintFloatMessage(std::move(message));
                 break;
             case 3:
-                ss << " - " << kStringType
-                   << " - " << std::string(message.content, kMaxContentLen);
+                PrettyPrintStringMessage(std::move(message));
                 break;
             default:
                 LOG_DEBUG("Case " + std::to_string(message.type) +
                         " not handled");
                 return;
         }
-
-        LOG_INFO(ss.str());
     }
 }
 
